44db3b427f6a9da784cea82b27b87be7c906e97b
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2026-02-02 00:19:34 +0100

    refactor: update Lesson01 function to return a string and remove unused array declaration

--- practice_service/main.go
+++ practice_service/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"practice_service/leetcode"
 )
 
 func main() {
 	// fmt.Println("Hello World")
 
-	// lessons.Lesson01()
+	// fmt.Println(lessons.Lesson01())
 	// lessons.Lesson02()
-	leetcode.IsPowerOfTwo(-16)
+	// leetcode.IsPowerOfTwo(-16)
+	fmt.Println(leetcode.NextGreatestLetter([]byte{'c', 'f', 'j'}, 'j'))
 }
